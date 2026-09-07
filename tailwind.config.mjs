9b56d6b874aac1af5ac3a342d104b2e0dba5da4e
Atulscript <getintouch.atul@gmail.com>
2026-09-07 18:26:34 +0530

    feat: complete UI/UX compact overhaul with Material 3 design and responsive modules

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -35,6 +35,8 @@ export default {
         }
       },
       boxShadow: {
+        '2xs': '0 1px 2px 0 rgba(0, 0, 0, 0.03)',
+        'xs': '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
         'm3-1': '0px 1px 3px 1px rgba(0, 0, 0, 0.08), 0px 1px 2px 0px rgba(0, 0, 0, 0.12)',
         'm3-2': '0px 2px 6px 2px rgba(0, 0, 0, 0.08), 0px 1px 2px 0px rgba(0, 0, 0, 0.12)',
         'm3-3': '0px 4px 8px 3px rgba(0, 0, 0, 0.08), 0px 1px 3px 0px rgba(0, 0, 0, 0.12)',
